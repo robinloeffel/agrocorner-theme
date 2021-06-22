693e7031baf2ecf17181baa3f0d285be01c6b04a
Robin Löffel <hi@robinloeffel.ch>
2021-06-22 17:20:59 +0200

    make background fixed via css

--- components/hero/hero.php
+++ components/hero/hero.php
@@ -1,6 +1,18 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
 
 <div class="hero">
+  <@ with @{ imageHero } @>
+    <style>
+      .hero {
+        background: url(@{ :file });
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+        background-repeat: no-repeat;
+      }
+    </style>
+  <@ end @>
+
 	<p class="hero-text">
 		<span class="hero-tagline">
       @{ tagline | def('Wir sind Ihr Partner für alle Gemüsesorten und helfen Ihnen von A wie Aubergine bis Z wie Zucchetti!') }
@@ -9,9 +21,6 @@
       @{ labelButton | def('Unsere Produkte') }
 		</a>
 	</p>
-  <@ with @{ imageHero } @>
-    <img src="@{ :file }" alt="@{ :caption }" class="hero-image">
-  <@ end @>
 
 	<script src="/packages/agrocorner-theme/components/hero/hero.js"></script>
 	<link rel="stylesheet" href="/packages/agrocorner-theme/components/hero/hero.css">
