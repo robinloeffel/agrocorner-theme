11857fbd550b4af187fa1fb70b0e565fdd57688a
Robin Löffel <hi@robinloeffel.ch>
2021-06-22 17:21:16 +0200

    finish up page-tiles

--- components/page-tiles/page-tiles.php
+++ components/page-tiles/page-tiles.php
@@ -1,24 +1,25 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
 
 <ul class="page-tiles">
-  <li class="page-tile">
-    <a class="page-link" href="#">Produkte</a>
-  </li>
-  <li class="page-tile">
-    <a class="page-link" href="#">Formulare</a>
-  </li>
-  <li class="page-tile">
-    <a class="page-link" href="#">Beratung</a>
-  </li>
-  <li class="page-tile">
-    <a class="page-link" href="#">Unternehmen</a>
-  </li>
-  <li class="page-tile">
-    <a class="page-link" href="#">Aktuelles</a>
-  </li>
-  <li class="page-tile">
-    <a class="page-link" href="#">Kontakt</a>
-  </li>
+  <@ newPagelist {
+    sort: 'date asc',
+    type: 'children'
+  } @>
+  <@foreach in pagelist @>
+    <li
+      class="
+        page-tile
+        <@if @{ checkboxSpanTwo }@>page-tile-span-two<@ end @>
+      "
+    >
+      <a class="page-link" href="@{ url }">
+        <span class="page-title">@{ title }</span>
+        <@ with @{ imageHero } @>
+          <img src="@{ :file }" alt="@{ :caption }" class="page-hero">
+        <@ end @>
+      </a>
+    </li>
+  <@ end @>
 </ul>
 
 <link rel="stylesheet" href="/packages/agrocorner-theme/components/page-tiles/page-tiles.css">
