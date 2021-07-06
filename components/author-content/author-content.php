e9f9fafb621ac8dc22b7565a26beaf74d3d34f0e
Robin Löffel <hi@robinloeffel.ch>
2021-07-06 14:41:14 +0200

    use blocks in author-content

--- components/author-content/author-content.php
+++ components/author-content/author-content.php
@@ -1,9 +1,9 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!') ?>
 
-<@ if @{ textAuthor } @>
+<@ if @{ +content } @>
 <div class="author-content">
   <div class="editable">
-    @{ textAuthor }
+    @{ +content }
   </div>
 </div>
 <link rel="stylesheet" href="/packages/agrocorner-theme/components/author-content/author-content.css">
