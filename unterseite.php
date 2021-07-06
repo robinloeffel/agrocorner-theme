cfb3582967379faf685d87e3b9704812a9d19806
Robin Löffel <hi@robinloeffel.ch>
2021-07-06 13:44:31 +0200

    add sub page template

---
+++ unterseite.php
@@ -0,0 +1,14 @@
+<?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
+
+<!doctype html>
+<html>
+
+<@ partials/head.php @>
+
+<body class="agrocorner">
+	<@ components/navigation/navigation.php @>
+  <@ components/banner/banner.php @>
+  <@ components/page-tiles/page-tiles.php @>
+  <@ components/footer/footer.php @>
+</body>
+</html>
