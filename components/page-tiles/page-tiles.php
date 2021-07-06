b882b6802838d77a2b5ed75ec142c5107923fb95
Robin Löffel <hi@robinloeffel.ch>
2021-07-06 13:52:31 +0200

    only render page-tiles if there's actually child pages

--- components/page-tiles/page-tiles.php
+++ components/page-tiles/page-tiles.php
@@ -1,24 +1,26 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
 
-<ul class="page-tiles">
-  <@ newPagelist {
-    sort: 'date asc',
-    type: 'children'
-  } @>
-  <@foreach in pagelist @>
-    <li
-      class="
-        page-tile
-        <@if @{ checkboxSpanTwo }@>page-tile-span-two<@ end @>
-      "
-    >
-      <a class="page-link" href="@{ url }">
-        <span class="page-title">@{ title }</span>
-        <@ with @{ imageHero } @>
-          <img src="@{ :file }" alt="@{ :caption }" class="page-hero">
-        <@ end @>
-      </a>
-    </li>
-  <@ end @>
-</ul>
-<link rel="stylesheet" href="/packages/agrocorner-theme/components/page-tiles/page-tiles.css">
+<@ newPagelist {
+  sort: 'date asc',
+  type: 'children'
+} @>
+<@ if @{ :pagelistCount } > 1 @>
+  <ul class="page-tiles">
+    <@foreach in pagelist @>
+      <li
+        class="
+          page-tile
+          <@if @{ checkboxSpanTwo }@>page-tile-span-two<@ end @>
+        "
+      >
+        <a class="page-link" href="@{ url }">
+          <span class="page-title">@{ title }</span>
+          <@ with @{ imageHero } @>
+            <img src="@{ :file }" alt="@{ :caption }" class="page-hero">
+          <@ end @>
+        </a>
+      </li>
+    <@ end @>
+  </ul>
+  <link rel="stylesheet" href="/packages/agrocorner-theme/components/page-tiles/page-tiles.css">
+<@ end @>
