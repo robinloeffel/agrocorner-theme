82075d8e44320f3f1fc21e37f370355d1cd71253
Robin Löffel <hi@robinloeffel.ch>
2021-07-06 13:44:25 +0200

    add banner component

---
+++ components/banner/banner.php
@@ -0,0 +1,15 @@
+<div class="banner">
+  <@ with @{ imageHero } @>
+    <style>
+      .banner {
+        background: url(@{ :file });
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+        background-repeat: no-repeat;
+      }
+    </style>
+  <@ end @>
+  lol
+</div>
+<link rel="stylesheet" href="/packages/agrocorner-theme/components/banner/banner.css">
