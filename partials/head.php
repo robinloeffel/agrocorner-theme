392b43592f5659385d70d0a2b46c1ac7329e4034
Robin Löffel <hi@robinloeffel.ch>
2021-07-13 14:39:09 +0200

    introduce ie 11 message

--- partials/head.php
+++ partials/head.php
@@ -15,5 +15,6 @@
   <link rel="stylesheet" href="https://unpkg.com/@csstools/normalize.css/evergreen.css">
 	<link rel="stylesheet" href="/packages/agrocorner-theme/agrocorner.css">
 	<script src="/packages/agrocorner-theme/agrocorner.js" defer></script>
+  <script src="/packages/agrocorner-theme/components/ie11/ie11.js" defer></script>
 	<title>@{ sitename } – @{ title }</title>
 </head>
