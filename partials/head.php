9726ff3fe8eebfd191554e32a6f51a3f9da533e2
Robin Löffel <hi@robinloeffel.ch>
2021-07-13 16:16:19 +0200

    improve ie 11 detection and redirection

--- partials/head.php
+++ partials/head.php
@@ -1,6 +1,7 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
 
 <head>
+  <title>@{ title } | @{ sitename }</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
@@ -15,6 +16,9 @@
   <link rel="stylesheet" href="https://unpkg.com/@csstools/normalize.css/evergreen.css">
 	<link rel="stylesheet" href="/packages/agrocorner-theme/agrocorner.css">
 	<script src="/packages/agrocorner-theme/agrocorner.js" defer></script>
-  <script src="/packages/agrocorner-theme/components/ie11/ie11.js" defer></script>
-	<title>@{ sitename } – @{ title }</title>
+  <script>
+    if (window.navigator.userAgent.indexOf('Trident') > -1) {
+      window.location = '/packages/agrocorner-theme/pages/ie11/ie11.html';
+    }
+  </script>
 </head>
