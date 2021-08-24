940cc6503c25d4883e22e6de5a3f5d6123e7fc96
Robin Löffel <hi@robinloeffel.ch>
2021-08-24 10:35:01 +0200

    add resource hints

--- partials/head.php
+++ partials/head.php
@@ -1,6 +1,11 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
 
 <head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="preload" href="https://unpkg.com/@csstools/normalize.css/evergreen.css" as="style">
+  <link rel="preload" href="/packages/agrocorner-theme/agrocorner.css" as="style">
+  <link rel="preload" href="/packages/agrocorner-theme/agrocorner.js" as="script">
   <title>@{ title } | @{ sitename }</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,7 +18,7 @@
 	  <link rel="shortcut icon" href="@{ :file }">
   <@ end @>
 	<link rel="canonical" href="https://agrocorner.ch">
-  <link rel="stylesheet" href="https://unpkg.com/@fontsource/inter/latin.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700&display=swap">
   <link rel="stylesheet" href="https://unpkg.com/@csstools/normalize.css/evergreen.css">
 	<link rel="stylesheet" href="/packages/agrocorner-theme/agrocorner.css">
 	<script src="/packages/agrocorner-theme/agrocorner.js" defer></script>
