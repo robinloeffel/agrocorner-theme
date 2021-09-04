e127ed36600192103e447abe5d5654cd28d4f0a1
Robin Löffel <hi@robinloeffel.ch>
2021-09-04 16:39:51 +0200

    improve splash message

--- components/splash-message/splash-message.js
+++ components/splash-message/splash-message.js
@@ -1,26 +1,38 @@
-const hideMessage = !!window.sessionStorage.getItem('hideSplashMessage');
 const message = document.querySelector('.splash-message');
 
-if (hideMessage) {
-  message.remove();
-} else {
-  const closeButton = document.querySelector('.splash-message-close');
+// check if there's a splash message on the dom
+if (message) {
+  if (window.sessionStorage.getItem('hideSplashMessage')) {
+    // if there is, but the user has already closed it before, just remove it
+    message.remove();
+  } else {
+    // if there is and the user hasn't clicked it away, initialize the module
+    const closeButton = document.querySelector('.splash-message-close');
 
-  const close = () => {
-    message.classList.add('splash-message-fading-out');
-    window.sessionStorage.setItem('hideSplashMessage', true);
+    const close = () => {
+      message.classList.add('splash-message-fading-out');
+      window.sessionStorage.setItem('hideSplashMessage', true);
 
-    message.addEventListener('transitionend', () => {
-      message.remove();
-    });
-  };
+      message.addEventListener('transitionend', () => {
+        message.remove();
+      });
+    };
 
-  const show = () => {
-    window.requestAnimationFrame(() => {
-      message.classList.add('splash-message-visible');
-    });
-  };
+    const closeViaKeyboard = event => {
+      if (event.key === 'Escape') {
+        window.removeEventListener('keydown', closeViaKeyboard);
+        close();
+      }
+    };
 
-  closeButton.addEventListener('click', close);
-  window.addEventListener('load', show);
+    const show = () => {
+      window.requestAnimationFrame(() => {
+        message.classList.add('splash-message-visible');
+      });
+    };
+
+    closeButton.addEventListener('click', close);
+    window.addEventListener('load', show);
+    window.addEventListener('keydown', closeViaKeyboard);
+  }
 }
