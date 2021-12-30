d75a8b16947ddee4b125d9e28667c17afda60caa
Robin Löffel <hi@robinloeffel.ch>
2021-12-30 16:11:45 +0100

    iron out issues with the splash message

--- source/modules/splash-message/splash-message.js
+++ source/modules/splash-message/splash-message.js
@@ -3,18 +3,21 @@ const message = document.querySelector('.splash-message');
 // check if there's a splash message on the dom
 if (message) {
   if (window.sessionStorage.getItem('hideSplashMessage')) {
-    // if there is, but the user has already closed it before, just remove it
+    // if there is, but the user has already closed it during this session, remove it
     message.remove();
   } else {
     // if there is and the user hasn't clicked it away, initialize the module
     const closeButton = document.querySelector('.splash-message-close');
 
     const close = () => {
-      message.classList.add('splash-message-fading-out');
       window.sessionStorage.setItem('hideSplashMessage', true);
 
       message.addEventListener('transitionend', () => {
         message.remove();
+      });
+
+      window.requestAnimationFrame(() => {
+        message.classList.remove('splash-message-visible');
       });
     };
 
@@ -32,7 +35,7 @@ if (message) {
     };
 
     closeButton.addEventListener('click', close);
-    window.addEventListener('load', show);
     window.addEventListener('keydown', closeViaKeyboard);
+    window.addEventListener('load', show);
   }
 }
