9d05e705485bcc97c09317b538bbe078b850f6ec
Robin Löffel <hi@robinloeffel.ch>
2022-01-18 09:46:48 +0100

    add empty default value for background-image

--- source/modules/banner/banner.php
+++ source/modules/banner/banner.php
@@ -1,7 +1,7 @@
 <header
   class="banner"
   style="
-    background-image: url(@{ imageBanner });
+    background-image: url(@{ imageBanner } | def(''));
     background-position: @{ bannerFocusPoint | def('center') };
   "
 ></header>
