29b31f494f1c5c0cc50d25728ecba60108266162
Robin Löffel <hi@robinloeffel.ch>
2022-01-18 15:11:41 +0100

    don't even show banners or hero if no image exists

--- source/modules/banner/banner.php
+++ source/modules/banner/banner.php
@@ -1,7 +1,11 @@
-<header
-  class="banner"
-  style="
-    background-image: url(@{ imageBanner | def('') });
-    background-position: @{ bannerFocusPoint | def('center') };
-  "
-></header>
+<?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
+
+<@ with @{ imageBanner } @>
+  <header
+    class="banner"
+    style="
+      background-image: url(@{ imageBanner });
+      background-position: @{ bannerFocusPoint | def('center') };
+    "
+  ></header>
+<@ end @>
--- source/modules/hero/hero.php
+++ source/modules/hero/hero.php
@@ -1,21 +1,23 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
 
-<header
-  class="
-    hero
-    <@ if !@{ tagline } @>hero-no-text<@ end @>
-  "
-  style="
-    background-image: url(@{ imageHero | def('') });
-    background-position: @{ heroFocusPoint | def('center') };
-  "
->
-  <@ if @{ tagline } @>
-    <div class="hero-text">
-      <h1 class="hero-tagline">@{ tagline }</h1>
-      <@ if @{ labelCtaButton } @>
-        <a href="@{ urlCtaButton }" class="hero-cta">@{ labelCtaButton }</a>
-      <@ end @>
-    </div>
-  <@ end @>
-</header>
+<@ with @{ imageHero } @>
+  <header
+    class="
+      hero
+      <@ if !@{ tagline } @>hero-no-text<@ end @>
+    "
+    style="
+      background-image: url(@{ imageHero });
+      background-position: @{ heroFocusPoint | def('center') };
+    "
+  >
+    <@ if @{ tagline } @>
+      <div class="hero-text">
+        <h1 class="hero-tagline">@{ tagline }</h1>
+        <@ if @{ labelCtaButton } @>
+          <a href="@{ urlCtaButton }" class="hero-cta">@{ labelCtaButton }</a>
+        <@ end @>
+      </div>
+    <@ end @>
+  </header>
+<@ end @>
